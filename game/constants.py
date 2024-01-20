952b330e9c264b0c41882d0f427520d28fca8ca9
Jasper Chua <s10177296@connect.np.edu.sg>
2024-01-20 17:21:52 +0800

    add game assets and constants

---
+++ game/constants.py
@@ -0,0 +1,43 @@
+import pygame
+
+IDLE_SPRITES = [
+    pygame.image.load('assets/monk/idle/idle_1.png'),
+    pygame.image.load('assets/monk/idle/idle_2.png'),
+    pygame.image.load('assets/monk/idle/idle_3.png'),
+    pygame.image.load('assets/monk/idle/idle_4.png'),
+    pygame.image.load('assets/monk/idle/idle_5.png'),
+    pygame.image.load('assets/monk/idle/idle_6.png'),
+]
+
+RUN_SPRITES = [
+    pygame.image.load('assets/monk/run/run_1.png'),
+    pygame.image.load('assets/monk/run/run_2.png'),
+    pygame.image.load('assets/monk/run/run_3.png'),
+    pygame.image.load('assets/monk/run/run_4.png'),
+    pygame.image.load('assets/monk/run/run_5.png'),
+    pygame.image.load('assets/monk/run/run_6.png'),
+    pygame.image.load('assets/monk/run/run_7.png'),
+    pygame.image.load('assets/monk/run/run_8.png'),
+]
+
+JUMP_UP_SPRITES = [
+    pygame.image.load('assets/monk/j_up/j_up_1.png'),
+    pygame.image.load('assets/monk/j_up/j_up_2.png'),
+    pygame.image.load('assets/monk/j_up/j_up_3.png'),
+]
+
+JUMP_DOWN_SPRITES = [
+    pygame.image.load('assets/monk/j_down/j_down_1.png'),
+    pygame.image.load('assets/monk/j_down/j_down_2.png'),
+    pygame.image.load('assets/monk/j_down/j_down_3.png'),
+]
+
+KICK_SPRITES = [
+    pygame.image.load('assets/monk/air_atk/air_atk_1.png'),
+    pygame.image.load('assets/monk/air_atk/air_atk_2.png'),
+    pygame.image.load('assets/monk/air_atk/air_atk_3.png'),
+    pygame.image.load('assets/monk/air_atk/air_atk_4.png'),
+    pygame.image.load('assets/monk/air_atk/air_atk_5.png'),
+    pygame.image.load('assets/monk/air_atk/air_atk_6.png'),
+    pygame.image.load('assets/monk/air_atk/air_atk_7.png'),
+]
